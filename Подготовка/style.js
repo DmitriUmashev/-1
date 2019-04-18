212780733579fafa070d4d27e738ba12b783cbf9
DmitriUmashev <49752572+DmitriUmashev@users.noreply.github.com>
2019-04-18 14:25:03 +0300

    Add files via upload

---
+++ Подготовка/style.js
@@ -0,0 +1,9 @@
+alert("Представление работы");
+var name= 'Дмитрий Юмашев';
+var text= "Меня зовут Дмитрий Юмашев";
+var a = 1234.56;
+var b = 78.90;
+var result = a + b;
+console.log(a + b);
+console.log(Math.round(result));
+document.write(text);
